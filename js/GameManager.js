0f00da836ab92642f8eadf3097ef632bf29e586e
Guillem Ortega <guillem212@hotmail.es>
2018-04-17 17:27:54 +0200

    Add files via upload

---
+++ js/GameManager.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+function preload() {
+    
+}
+
+function create(){
+    
+}
+
+function update(){
+    
+}
